450ca8e5e0e20af008fefde1b84ea3afbd05eada
Narmin Gezalova <nermingozelova04@gmail.com>
2025-09-13 01:10:11 +0400

    Array Methods Lesson

---
+++ src/ArraysSmth.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArrayMethod {
+    public  static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = {6, 7, 8, 9, 10};
+
+        int[] array2 = {1, 2, 3, 4, 5};
+
+        for(int element : array){
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+
+        for(int element : array2){
+            System.out.print(element + " ");
+        }
+
+
+
+
+    }
+}
