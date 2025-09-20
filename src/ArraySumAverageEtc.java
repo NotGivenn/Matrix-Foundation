59d04a1e52153854502b56248fcb8d82a1fb222e
NotGivenn <ngezalova95@mail.ru>
2025-09-20 16:56:17 +0400

    sum of array

---
+++ src/ArraySumAverageEtc.java
@@ -0,0 +1,58 @@
+public class ArraySumAverageEtc {
+
+        //4
+
+//    static double averageArray(int[] array) {
+//
+//        int summ = 0;
+//
+//        for (int a = 0; a < array.length; a++) {
+//            summ = summ + array[a];
+//
+//        }
+//
+//        return summ / array.length;
+//
+//    }
+
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+
+
+        //1
+//        int allSum = 0;
+//        for (int i = 0; i < array.length; i++) {
+//
+//            allSum = allSum + array[i];
+//        }
+//        System.out.println(allSum);
+
+
+        //2
+//        int evenSum = 0;
+//
+//        for (int k = 0; k < array.length; k += 2) {
+//            evenSum = evenSum + array[k];
+//        }
+//        System.out.print(evenSum);
+
+
+        //3
+//        int evenSumIf = 0;
+//        for(int j =0; j<array.length; j++){
+//            if(array[j]%2 ==0){
+//                evenSumIf +=array[j];
+//            }
+//        }
+//        System.out.println(evenSumIf);
+
+
+        //4
+
+//        System.out.println(averageArray(array));
+
+
+
+    }
+}
